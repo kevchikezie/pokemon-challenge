4d03af8759690b7b5604438f041db13c5100a7e4
Kelvin Chikezie <kevchike@gmail.com>
2022-03-05 17:08:47 +0100

    Adjust namespace for api and web controllers in RouteServiceProvider

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -26,7 +26,8 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string|null
      */
-    // protected $namespace = 'App\\Http\\Controllers';
+    protected $webNamespace = 'App\\Http\\Controllers\\Web';
+    protected $apiNamespace = 'App\\Http\\Controllers\\Api';
 
     /**
      * Define your route model bindings, pattern filters, etc.
@@ -40,11 +41,11 @@ class RouteServiceProvider extends ServiceProvider
         $this->routes(function () {
             Route::prefix('api')
                 ->middleware('api')
-                ->namespace($this->namespace)
+                ->namespace($this->apiNamespace)
                 ->group(base_path('routes/api.php'));
 
             Route::middleware('web')
-                ->namespace($this->namespace)
+                ->namespace($this->webNamespace)
                 ->group(base_path('routes/web.php'));
         });
     }
