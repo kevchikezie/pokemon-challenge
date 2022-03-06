18c0d7331213abb1e8964a3d58241dc5940cd2c0
Kelvin Chikezie <kevchike@gmail.com>
2022-03-06 18:07:16 +0100

    API endpoint to get all pokemon

---
+++ app/Http/Controllers/Api/PokemonController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Illuminate\Http\Request;
+use App\Services\PokemonService;
+use App\Http\Controllers\Controller;
+
+
+class PokemonController extends Controller
+{
+    /**
+     * Class properties
+     * 
+     * @property 
+     */
+    private $pokemonService;
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct(PokemonService $pokemonService) 
+    {
+        $this->pokemonService = $pokemonService;
+    }
+
+    /**
+     * List all pokemon
+     * 
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+        $pokemons = $this->pokemonService->paginatedRecords();
+
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Fetched pokemons successfully',
+            'data' => $pokemons
+        ], 200);
+    }
+}
